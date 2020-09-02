3a0c74e62a93ed04942a3d923ff0c07c71d7b693
miguelrodfher <miguelrodfher@gmail.com>
2020-09-02 19:16:22 -0400

    fixed error on Employer addition

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/controllers/SkillController.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/controllers/SkillController.java
@@ -18,6 +18,10 @@ public class SkillController {
     @Autowired
     private SkillRepository skillRepository;
 
+    @GetMapping("")
+    public String displayAllJobs(Model model) {
+        return "skills/index";
+    }
     @GetMapping("add")
     public String displayAddSkillForm(Model model) {
         model.addAttribute(new Skill());
